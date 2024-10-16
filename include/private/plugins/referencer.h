90af077adf67ebe261fa084bb826f4a2159b6fcb
Vladimir Sadovnikov <sadko4u@gmail.com>
2024-10-16 20:43:50 +0300

    Initial import from template

---
+++ include/private/plugins/referencer.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2024 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2024 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-referencer
+ * Created on: 16 окт 2024 г.
+ *
+ * lsp-plugins-referencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-referencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-referencer. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_PLUGINS_REFERENCER_H_
+#define PRIVATE_PLUGINS_REFERENCER_H_
+
+#include <lsp-plug.in/dsp-units/util/Delay.h>
+#include <lsp-plug.in/dsp-units/ctl/Bypass.h>
+#include <lsp-plug.in/plug-fw/plug.h>
+#include <private/meta/referencer.h>
+
+namespace lsp
+{
+    namespace plugins
+    {
+        /**
+         * Base class for the latency compensation delay
+         */
+        class referencer: public plug::Module
+        {
+            protected:
+                enum mode_t
+                {
+                    CD_MONO,
+                    CD_STEREO,
+                    CD_X2_STEREO
+                };
+
+                typedef struct channel_t
+                {
+                    // DSP processing modules
+                    dspu::Delay         sLine;              // Delay line
+                    dspu::Bypass        sBypass;            // Bypass
+
+                    // Parameters
+                    ssize_t             nDelay;             // Actual delay of the signal
+                    float               fDryGain;           // Dry gain (unprocessed signal)
+                    float               fWetGain;           // Wet gain (processed signal)
+
+                    // Input ports
+                    plug::IPort        *pIn;                // Input port
+                    plug::IPort        *pOut;               // Output port
+                    plug::IPort        *pDelay;             // Delay (in samples)
+                    plug::IPort        *pDry;               // Dry control
+                    plug::IPort        *pWet;               // Wet control
+
+                    // Output ports
+                    plug::IPort        *pOutDelay;          // Output delay time
+                    plug::IPort        *pInLevel;           // Input signal level
+                    plug::IPort        *pOutLevel;          // Output signal level
+                } channel_t;
+
+            protected:
+                size_t              nChannels;          // Number of channels
+                channel_t          *vChannels;          // Delay channels
+                float              *vBuffer;            // Temporary buffer for audio processing
+
+                plug::IPort        *pBypass;            // Bypass
+                plug::IPort        *pGainOut;           // Output gain
+                plug::IPort        *pComment;           // Comment
+
+                uint8_t            *pData;              // Allocated data
+
+            protected:
+                void                do_destroy();
+
+            public:
+                explicit referencer(const meta::plugin_t *meta);
+                referencer (const referencer &) = delete;
+                referencer (referencer &&) = delete;
+                virtual ~referencer() override;
+
+                referencer & operator = (const referencer &) = delete;
+                referencer & operator = (referencer &&) = delete;
+
+                virtual void        init(plug::IWrapper *wrapper, plug::IPort **ports) override;
+                virtual void        destroy() override;
+
+            public:
+                virtual void        update_sample_rate(long sr) override;
+                virtual void        update_settings() override;
+                virtual void        process(size_t samples) override;
+                virtual void        dump(dspu::IStateDumper *v) const override;
+        };
+
+    } /* namespace plugins */
+} /* namespace lsp */
+
+
+#endif /* PRIVATE_PLUGINS_REFERENCER_H_ */
+
